48f67f0a571c9094b33c4db7c6f2790cd4888ede
Matteo Comisso <matteo.comisso@me.com>
2016-10-02 21:36:50 -0700

    Initial Commit

---
+++ budget-expense/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  budget-expense
+//
+//  Created by Matteo Comisso on 02/10/16.
+//  Copyright © 2016 mcomisso. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ budget-expense/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  budget-expense
+//
+//  Created by Matteo Comisso on 02/10/16.
+//  Copyright © 2016 mcomisso. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
