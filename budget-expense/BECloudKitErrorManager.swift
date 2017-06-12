7a1e25be8f5de2577c1274352b932ce556b0d6cf
Matteo Comisso <matteo.comisso@me.com>
2017-06-13 00:44:05 +0200

    Using appcoordinator

---
+++ budget-expense/BECloudKitErrorManager.swift
@@ -0,0 +1,9 @@
+//
+//  BECloudKitErrorManager.swift
+//  budget-expense
+//
+//  Created by Matteo Comisso on 12/06/2017.
+//  Copyright © 2017 mcomisso. All rights reserved.
+//
+
+import Foundation
