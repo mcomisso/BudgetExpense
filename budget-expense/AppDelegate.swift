2d1bba50f98a161cedfe670f156c5e98ed4141a5
Matteo Comisso <matteo.comisso@me.com>
2017-06-13 00:44:14 +0200

    AppCoordinator initial implementation

--- budget-expense/AppDelegate.swift
+++ budget-expense/AppDelegate.swift
@@ -13,12 +13,20 @@ import CoreLocation
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var coordinator: BEAppCoordinator?
+
     var window: UIWindow?
 
     let locationManager = BELocationManager()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        self.coordinator = BEAppCoordinator()
+
+        window = UIWindow()
+        window?.rootViewController = self.coordinator?.rootViewController
+        window?.makeKeyAndVisible()
 
         // Location to fetch
         self.locationManager.requestAuthorization()
--- budget-expense/BEAppCoordinator.swift
+++ budget-expense/BEAppCoordinator.swift
@@ -7,3 +7,54 @@
 //
 
 import Foundation
+import UIKit
+import SQFeedbackGenerator
+
+
+class BEAppCoordinator: BEHomeViewControllerDelegate {
+
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+
+    var rootViewController: UIViewController?
+
+    init() {
+        self.rootViewController = storyboard.instantiateInitialViewController()
+
+        if let rootVC = self.rootViewController as? BEHomeViewController {
+            rootVC.delegate = self
+        }
+    }
+
+    //MARK:- HOMEVIEWCONTROLLER
+    func didSelectExpenseScreen(_ homeViewController: BEHomeViewController) {
+        // Expense screen loading
+
+        guard let addDataVC = self.storyboard.instantiateViewController(withIdentifier: BEConstants.Identifiers.addDataViewController) as? BEAddDataViewController else { return }
+
+        addDataVC.type = .expense
+        homeViewController.presentAnimator = BETransitioningPresentingAnimator(direction: .up)
+        addDataVC.transitioningDelegate = homeViewController
+
+        homeViewController.present(addDataVC, animated: true) { 
+            SQFeedbackGenerator().generateFeedback(type: .notification)
+        }
+    }
+
+
+    func didSelectListScreen(_ homeViewController: BEHomeViewController) {
+        homeViewController.performSegue(withIdentifier: "BEViewTransactionsSegue", sender: nil)
+    }
+
+    func didSelectIncomeScreen(_ homeViewController: BEHomeViewController) {
+        guard let addDataVC = self.storyboard.instantiateViewController(withIdentifier: BEConstants.Identifiers.addDataViewController) as? BEAddDataViewController else { return }
+
+        addDataVC.type = .income
+        homeViewController.presentAnimator = BETransitioningPresentingAnimator(direction: .down)
+        addDataVC.transitioningDelegate = homeViewController
+
+        homeViewController.present(addDataVC, animated: true) {
+            SQFeedbackGenerator().generateFeedback(type: .notification)
+        }
+    }
+
+}
