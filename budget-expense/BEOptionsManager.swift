3fe034a783cf1ad0365cb2eac9a5091ba51cf796
Matteo Comisso <matteo.comisso@me.com>
2017-06-14 00:13:17 +0200

    Settings manager

--- budget-expense/BEOptionsManager.swift
+++ budget-expense/BEOptionsManager.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import UIKit
+
+enum BESettings: String {
+    case hapticFeedbackEnabled
+    case appSoundsEnabled
+}
+
+final class BESettingsManager {
+
+    static func initializeDefaults() {
+        UserDefaults.standard.safeSet(true, forKey: BESettings.hapticFeedbackEnabled.rawValue)
+        UserDefaults.standard.safeSet(true, forKey: BESettings.appSoundsEnabled.rawValue)
+    }
+
+    static func readSettingForKey(key: BESettings) -> Bool {
+        return UserDefaults.standard.bool(forKey: key.rawValue)
+    }
+
+}
+
+extension UserDefaults {
+    func safeSet(_ value: Bool, forKey: String) {
+        if self.object(forKey: forKey) == nil {
+            self.set(value, forKey: forKey)
+        }
+    }
+}
