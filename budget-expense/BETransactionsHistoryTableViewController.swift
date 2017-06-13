19d4631143160078a29d765a1f9bd32212556154
Matteo Comisso <matteo.comisso@me.com>
2017-06-13 19:16:47 +0200

    CollectionView instead of Tableview for transactions

--- budget-expense/BETransactionsHistoryTableViewController.swift
+++ budget-expense/BETransactionsHistoryTableViewController.swift
@@ -10,60 +10,24 @@ import UIKit
 import RealmSwift
 import Material
 
-class BEHeaderView: UITableViewHeaderFooterView {
-
-    // Add a date
-}
-
-class BETableViewCell: UITableViewCell {
-
-    @IBOutlet weak var card: Card!
-
-    func prepareCardWithAmounObject(amount: Amount) {
-        let amountLabel = UILabel()
-        amountLabel.font = UIFont.systemFont(ofSize: 40)
-        amountLabel.translatesAutoresizingMaskIntoConstraints = false
-        amountLabel.numberOfLines = 1
-        amountLabel.textAlignment = .center
-        amountLabel.text = BEUtils.formatNumberToCurrency(number: NSNumber(value: amount.amount))
-        amountLabel.textColor = amount.isExpense ? BETheme.Colors.accent : BETheme.Colors.primary
-
-        let verticalIconButton = IconButton(image: Icon.cm.moreVertical, tintColor: Color.blueGrey.base)
-
-
-        let toolbar = Toolbar(rightViews: [verticalIconButton])
-        toolbar.detail = amount.isExpense ? "Expense" : "Income"
-        toolbar.detailLabel.textAlignment = .left
-        toolbar.title = amount.category?.name
-        toolbar.titleLabel.textAlignment = .left
-
-        let dateLabel = UILabel()
-        dateLabel.font = RobotoFont.regular(with: 12)
-        dateLabel.textColor = Color.blueGrey.base
-        dateLabel.text = BEUtils().dateFormatter.string(from: amount.date)
-
-        let bottomBar = Bar(leftViews: [dateLabel])
-
-        card.toolbar = toolbar
-        card.contentView = amountLabel
-        card.bottomBar = bottomBar
-    }
-
-    override func prepareForReuse() {
-        super.prepareForReuse()
-    }
-}
-
-
-class BETransactionsHistoryTableViewController: UITableViewController {
+class BETransactionsHistoryCollectionViewController: UICollectionViewController {
 
     @IBOutlet weak var closeButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.title = "Transactions"
+
         self.closeButton.title = ""
         self.closeButton.image = Icon.close
+
+        if let collectionLayout = self.collectionView?.collectionViewLayout as? UICollectionViewFlowLayout {
+            collectionLayout.estimatedItemSize = CGSize(width: self.view.frame.size.width, height: 160)
+
+            collectionLayout.headerReferenceSize = CGSize(width: self.view.frame.size.width, height: 76)
+            collectionLayout.sectionHeadersPinToVisibleBounds = true
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -77,67 +41,32 @@ class BETransactionsHistoryTableViewController: UITableViewController {
 
 }
     // MARK: - Table view data source
-extension BETransactionsHistoryTableViewController {
+extension BETransactionsHistoryCollectionViewController {
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return BERealmManager.shared.getWeekDataForTableView().count
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: BEConstants.Identifiers.overviewCellIdentifier, for: indexPath) as! BETableViewCell
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BEConstants.Identifiers.overviewCellIdentifier, for: indexPath) as! BECollectionViewCell
 
         let amountObject = BERealmManager.shared.getWeekDataForTableView()[indexPath.row]
 
-        cell.prepareCardWithAmounObject(amount: amountObject)
+        cell.prepareCard(amount: amountObject)
 
         return cell
     }
+
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let reusableView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: BEHeaderView.reuseIdentifier, for: indexPath) as! BEHeaderView
+
+        reusableView.setDay(date: Date())
+
+        return reusableView
+    }
 }
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
