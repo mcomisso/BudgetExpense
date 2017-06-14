e508f5db884e9fb36f11640ef9b6a359754cf7dd
Matteo Comisso <matteo.comisso@me.com>
2017-06-14 19:07:09 +0200

    Gradient in transactioncell

--- budget-expense/BETransationCell.swift
+++ budget-expense/BETransationCell.swift
@@ -9,15 +9,23 @@
 import Foundation
 import UIKit
 import Material
+import Iconic
 
 enum CellButtonActionType: Int {
     case share = 101
     case options = 202
 }
 
-class BECollectionViewCell: UICollectionViewCell {
+protocol BETransactionCellDelegate: class {
+    func didPressOption(cell: BETransactionCollectionViewCell, amount: Amount?)
+    func didPressShare(cell: BETransactionCollectionViewCell, amount: Amount?)
+}
+
+class BETransactionCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var card: Card!
+
+    weak var delegate: BETransactionCellDelegate?
 
     var amount: Amount?
 
@@ -30,6 +38,8 @@ class BECollectionViewCell: UICollectionViewCell {
         return l
     }()
 
+    var gradiendLayer: CAGradientLayer? = nil
+
     func prepareCard(amount: Amount) {
 
         self.amount = amount
@@ -37,22 +47,26 @@ class BECollectionViewCell: UICollectionViewCell {
         guard let actualAmount = self.amount else { return }
 
         amountLabel.text = NSNumber(value: actualAmount.amount).toCurrency()
-        amountLabel.textColor = actualAmount.isExpense ? BETheme.Colors.accent : BETheme.Colors.primary
+        amountLabel.textColor = .white
 
         let verticalIconButton = IconButton(image: Icon.cm.moreVertical, tintColor: Color.blueGrey.base)
         verticalIconButton.tag = CellButtonActionType.options.rawValue
         verticalIconButton.addTarget(self, action: #selector(didSelectButton(_:)), for: .touchUpInside)
 
-        let toolbar = Toolbar(rightViews: [verticalIconButton])
-        toolbar.detail = actualAmount.isExpense ? "Expense" : "Income"
-        toolbar.detailLabel.textAlignment = .left
-        toolbar.title = actualAmount.category?.name ?? "Test"
-        toolbar.titleLabel.textAlignment = .left
+
+
+
+        let toolbar = Toolbar(leftViews: [UIImageView.init(image: Iconic.standardDimension(icon: .planeIcon, size: CGSize.init(width: 15, height: 15), color: .white))])
+//        Toolbar(rightViews: [verticalIconButton])
+//        toolbar.detail = actualAmount.isExpense ? "Expense" : "Income"
+//        toolbar.detailLabel.textAlignment = .left
+//        toolbar.title = actualAmount.category?.name ?? "Test"
+//        toolbar.titleLabel.textAlignment = .left
 
         let dateLabel = UILabel()
         dateLabel.font = RobotoFont.regular(with: 12)
         dateLabel.textColor = Color.blueGrey.base
-        dateLabel.text = BEUtils().dateFormatter.string(from: amount.date)
+        dateLabel.text = BEUtils.dateFormatter.string(from: amount.date)
 
         let shareButton = IconButton(image: Icon.share, tintColor: Color.blueGrey.base)
         shareButton.tag = CellButtonActionType.share.rawValue
@@ -62,11 +76,22 @@ class BECollectionViewCell: UICollectionViewCell {
         notesText.font = RobotoFont.regular(with: 12)
         notesText.text = self.amount?.notes
 
-        let bottomBar = Bar(leftViews: [dateLabel], rightViews: [shareButton], centerViews: [notesText])
+        let bottomBar = Bar(leftViews: [dateLabel], rightViews: [shareButton, verticalIconButton], centerViews: [notesText])
 
         card.toolbar = toolbar
         card.contentView = amountLabel
+
+        if amount.isExpense {
+            card.container.gradientFromColor(BETheme.Colors.expense)
+        } else {
+            card.container.gradientFromColor(BETheme.Colors.income)
+        }
+
         card.bottomBar = bottomBar
+
+        card.toolbar?.backgroundColor = .clear
+        card.contentView?.backgroundColor = .clear
+        card.bottomBar?.backgroundColor = .clear
     }
 
     func didSelectButton(_ sender: Any) {
@@ -77,14 +102,18 @@ class BECollectionViewCell: UICollectionViewCell {
         case .options:
             // Vertical button
             print("Pressed options")
+            self.delegate?.didPressOption(cell: self, amount: self.amount)
+
         case .share:
             // Share button
             print("Pressed share")
+            self.delegate?.didPressShare(cell: self, amount: self.amount)
         }
     }
 
     override func prepareForReuse() {
         super.prepareForReuse()
+        self.gradiendLayer = nil
         self.amountLabel.text = nil
         self.amount = nil
     }
