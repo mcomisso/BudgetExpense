17703fc481dca1804ab0686b47fffd529fbf55d1
Matteo Comisso <matteo.comisso@me.com>
2017-06-14 19:06:11 +0200

    Category gradient

--- budget-expense/BECategoryColorCell.swift
+++ budget-expense/BECategoryColorCell.swift
@@ -10,19 +10,23 @@ import UIKit
 
 final class BEColorCell: UICollectionViewCell {
 
+    private var gradientLayer: CAGradientLayer? = nil
+
     override func awakeFromNib() {
         super.awakeFromNib()
+
+        self.contentView.backgroundColor = .clear
 
         self.contentView.layer.cornerRadius = self.frame.width / 2.0
         self.contentView.clipsToBounds = true
     }
 
     func setColor(_ color: UIColor) {
-        self.contentView.backgroundColor = color
+        self.gradientLayer = self.contentView.gradientFromColor(color)
     }
 
     override func prepareForReuse() {
         super.prepareForReuse()
-        self.contentView.backgroundColor = .clear
+        self.gradientLayer?.removeFromSuperlayer()
     }
 }
