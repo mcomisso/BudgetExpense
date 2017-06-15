335731a4d564e33e7d041bd444db5a786d1b3aaf
Matteo Comisso <matteo.comisso@me.com>
2017-06-15 22:10:51 +0200

    Network manager to download

--- budget-expense/BENetworkManager.swift
+++ budget-expense/BENetworkManager.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+final class BENetworkManager: NSObject, URLSessionDataDelegate {
+
+    let session = URLSession(configuration: .default)
+
+    func getData(url: URL) {
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData)
+        session.dataTask(with: request)
+    }
+
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        //
+
+        do {
+            let json = try JSONSerialization.jsonObject(with: data, options: [])
+            print(json)
+        } catch {
+            print(error.localizedDescription)
+        }
+
+    }
+}
