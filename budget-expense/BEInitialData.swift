faa869eaee8c0aea115d18d2ea05180ebc73663e
Matteo Comisso <matteo.comisso@me.com>
2017-06-15 14:45:00 +0200

    Initial categories

--- budget-expense/BEInitialData.swift
+++ budget-expense/BEInitialData.swift
@@ -12,11 +12,11 @@ import Material
 
 struct BEInitialData {
     struct Categories {
-        static let travel = BECategory(gliph: FontAwesomeIcon.planeIcon.name, color: Color.indigo.base)
-        static let business = BECategory(gliph: FontAwesomeIcon.buildingIcon.name, color: Color.blueGrey.base)
-        static let eat = BECategory(gliph: FontAwesomeIcon.foodIcon.name, color: Color.deepOrange.base)
-        static let home = BECategory(gliph: FontAwesomeIcon.homeIcon.name, color: Color.brown)
-        static let appStore = BECategory(gliph: FontAwesomeIcon.appleIcon.name, color: Color.blue)
+        static let travel = BECategory(gliph: FontAwesomeIcon.planeIcon.name, color: Color.indigo.base, name: "Travel")
+        static let business = BECategory(gliph: FontAwesomeIcon.buildingIcon.name, color: Color.blueGrey.base, name: "Eusiness")
+        static let eat = BECategory(gliph: FontAwesomeIcon.foodIcon.name, color: Color.deepOrange.base, name: "Food")
+        static let home = BECategory(gliph: FontAwesomeIcon.homeIcon.name, color: Color.brown, name: "Home")
+        static let appStore = BECategory(gliph: FontAwesomeIcon.appleIcon.name, color: Color.blue, name: "appStore")
     }
 
     static func loadIntoRealm() {
