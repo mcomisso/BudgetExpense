6918b7d231e13298378995f156ca5d924ca66e46
Matteo Comisso <matteo.comisso@me.com>
2017-06-18 13:00:05 +0200

    Acknowledgments and improvements in section

--- budget-expense/BESettingsViewController.swift
+++ budget-expense/BESettingsViewController.swift
@@ -9,32 +9,58 @@ import UIKit
 import Foundation
 import Eureka
 import StoreKit
+import Material
+import AcknowList
 
 final class BESettingsViewController: FormViewController {
 
+    @IBOutlet weak var closeBarButton: UIBarButtonItem!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        form
-            +++ Section("Section1")
-            <<< TextRow(){ row in
+        self.title = "Settings"
+        self.closeBarButton.image = Icon.close
+        self.closeBarButton.title = ""
+
+        //MARK: GENERAL
+        /*
+         In-App Purchase
+         iCloud
+
+         */
+
+        form +++ Section("General")
+            <<< PhoneRow(){ row in
                 row.title = "Text Row"
                 row.placeholder = "Enter text here"
             }
-            <<< PhoneRow(){
-                $0.title = "Phone Row"
-                $0.placeholder = "And numbers here"
-            }
-            +++ Section("Currency")
-            <<< DateRow(){
-                $0.title = "Date Row"
-                $0.value = Date(timeIntervalSinceReferenceDate: 0)
-            }
-            +++ Section("Credits")
-            <<< PushRow<String>() {
-                $0.title = "Available currencies"
-                $0.options = BERealmManager.shared.listCurrencies()
-                $0.selectorTitle = "Select currency" }
-            +++ Section("Support")
+            <<< SwitchRow() { row in
+                row.title = "iCloud Enabled"
+                row.value = BESettings.appSoundsEnabled.boolValue
+        }
+
+
+        //MARK: REVIEW
+
+        form +++ Section()
+        if #available(iOS 10.3, *) {
+            form.last! <<< ButtonRow() {
+                $0.title = "Write a review"
+                }.onCellSelection({ (cell, row) in
+                    SKStoreReviewController.requestReview()
+                })
+        }
+        form.last!
+            <<< ButtonRow() {
+                $0.title = "Send feedback"
+                }.onCellSelection({ (cell, row) in
+                    // Open
+                })
+
+
+        //MARK: SUPPORT
+
+        form +++ Section("Support")
             <<< URLRow() {
                 $0.title = "website"
                 $0.value = URL(string: "http://mcomisso.xyz")!
@@ -46,30 +72,75 @@ final class BESettingsViewController: FormViewController {
             <<< TwitterRow() {
                 $0.title = "Twitter"
                 $0.value = "@teomatteo89"
-            }
-        if #available(iOS 10.3, *) {
-        form +++ Section()
-            <<< ButtonRow() {
-                $0.title = "Write a review"
-            }.onCellSelection({ (cell, row) in
-                SKStoreReviewController.requestReview()
-            })
         }
-        form +++ Section()
-        <<< ButtonRow() {
-            $0.title = "Reset all data"
-        }.onCellSelection({[weak self] (cell, row) in
-            let actionsheet = UIAlertController.init(title: "Are you sure?", message: "There is no recovery for this option", preferredStyle: .actionSheet)
-            actionsheet.addAction(UIAlertAction.init(title: "Yes, delete all my data", style: .destructive, handler: { (action) in
-                BERealmManager.shared.deleteAllData()
-            }))
-            actionsheet.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
-            self?.present(actionsheet, animated: true, completion: nil)
-        })
 
+
+        //MARK: WARNING ZONE
+
+        form +++ Section("Warning zone")
+            <<< ButtonRow() {
+                $0.title = "Reset all data"
+                }.onCellSelection({[weak self] (cell, row) in
+                    let actionsheet = UIAlertController.init(title: "Are you sure?", message: "There is no recovery for this option", preferredStyle: .actionSheet)
+                    actionsheet.addAction(UIAlertAction.init(title: "Yes, delete all my data", style: .destructive, handler: { (action) in
+                        BERealmManager.shared.deleteAllData()
+                    }))
+                    actionsheet.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
+                    self?.present(actionsheet, animated: true, completion: nil)
+                })
+
+
+        //MARK: SHARE
+
+        form +++ Section("Share")
+            <<< ButtonRow() {
+                $0.title = "Email"
+                }.onCellSelection({ (cell, row) in
+                    // Share on twitter
+                })
+            <<< ButtonRow() {
+                $0.title = "Whatsapp"
+                }.onCellSelection({ (cell, row) in
+                    // Share on twitter
+                })
+            <<< ButtonRow() {
+                $0.title = "Twitter"
+                }.onCellSelection({ (cell, row) in
+                    // Share on twitter
+                })
+            <<< ButtonRow() {
+                $0.title = "Facebook"
+                }.onCellSelection({ (cell, row) in
+                    // Share on twitter
+                })
+
+        //MARK: CREDITS
+
+        form +++ Section("Credits")
+            <<< PushRow<String>() {
+                $0.title = "Available currencies"
+                $0.options = BERealmManager.shared.listCurrencies()
+                $0.selectorTitle = "Select currency" }
+            <<< ButtonRow() {
+                $0.title = "Acknowledgments"
+                $0.presentationMode = PresentationMode.show(controllerProvider: ControllerProvider.callback(builder: { () -> AcknowListViewController in
+                    let path = Bundle.main.path(forResource: "Pods-budget-expense-acknowledgements", ofType: "plist")
+                    let cv = AcknowListViewController.init(acknowledgementsPlistPath: path)
+                    return cv
+                }), onDismiss: nil)
+
+        }
 
         self.navigationOptions = RowNavigationOptions.Enabled.union(.StopDisabledRow)
         self.animateScroll = true
         self.rowKeyboardSpacing = 20
+    }
+
+    @IBAction func closeAction(_ sender: Any) {
+        self.closeViewController()
+    }
+    
+    func closeViewController() {
+        self.dismiss(animated: true, completion: nil)
     }
 }
