03a538b57a3dd72346c73e5a9317a5782c6d9c37
Matteo Comisso <matteo.comisso@me.com>
2017-06-19 17:08:55 +0200

    Lighter colors on initial data

--- budget-expense/BEInitialData.swift
+++ budget-expense/BEInitialData.swift
@@ -12,11 +12,11 @@ import Material
 
 struct BEInitialData {
     struct Categories {
-        static let travel = BECategory(gliph: FontAwesomeIcon.planeIcon.name, color: Color.indigo.base, name: "Travel")
-        static let business = BECategory(gliph: FontAwesomeIcon.buildingIcon.name, color: Color.blueGrey.base, name: "Eusiness")
-        static let eat = BECategory(gliph: FontAwesomeIcon.foodIcon.name, color: Color.deepOrange.base, name: "Food")
-        static let home = BECategory(gliph: FontAwesomeIcon.homeIcon.name, color: Color.brown, name: "Home")
-        static let appStore = BECategory(gliph: FontAwesomeIcon.appleIcon.name, color: Color.blue, name: "appStore")
+        static let travel = BECategory(gliph: FontAwesomeIcon.planeIcon.name, color: Color.indigo.lighten2, name: "Travel")
+        static let business = BECategory(gliph: FontAwesomeIcon.buildingIcon.name, color: Color.blueGrey.lighten2, name: "Business")
+        static let eat = BECategory(gliph: FontAwesomeIcon.foodIcon.name, color: Color.deepOrange.lighten2, name: "Food")
+        static let home = BECategory(gliph: FontAwesomeIcon.homeIcon.name, color: Color.amber.lighten2, name: "Home")
+        static let appStore = BECategory(gliph: FontAwesomeIcon.appleIcon.name, color: Color.blue.lighten2, name: "appStore")
     }
 
     static func loadIntoRealm() {
