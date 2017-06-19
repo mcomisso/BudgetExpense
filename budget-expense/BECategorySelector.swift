0fa7c9781f930bf850c04067420decff6d0fe935
Matteo Comisso <matteo.comisso@me.com>
2017-06-20 00:09:07 +0200

    Category creation

--- budget-expense/BECategorySelector.swift
+++ budget-expense/BECategorySelector.swift
@@ -18,16 +18,16 @@ enum CategoryCollectionViewType: Int {
 
 final class BECategorySelectorViewController: UIViewController {
 
-    class GeneratedCategory {
-        private var gliph: UIImage?
+    final class GeneratedCategory {
+        private var gliph: String?
         private var color: UIColor = .lightGray
 
         func setColor(color: UIColor) {
             self.color = color
         }
 
-        func setImage(image: UIImage) {
-            self.gliph = image
+        func setGliph(iconName: String) {
+            self.gliph = iconName
         }
 
         func validate() -> Bool {
@@ -37,28 +37,31 @@ final class BECategorySelectorViewController: UIViewController {
             return true
         }
 
-        func imageRepresentation() -> UIImage? {
-            return self.gliph?.tint(with: self.color)
+        func toCategoryObject() -> BECategory? {
+            if self.validate() {
+                return BECategory.init(gliph: self.gliph!, color: self.color, name: self.gliph!)
+            }
+            return nil
         }
     }
 
     var newCategory: GeneratedCategory = GeneratedCategory()
 
     @IBOutlet weak var stackView: UIStackView!
-    
+
     @IBOutlet weak var collectionView: UICollectionView!
 
     @IBOutlet weak var collectionViewColors: UICollectionView!
 
-    let colorList: [UIColor] = [.white, .red, .green, .blue, .yellow, .purple, .cyan, .black, .orange]
+    let colorList: [UIColor] = [Color.amber.lighten2, Color.amber.darken1, Color.blue.lighten2, Color.blue.darken2, Color.brown.lighten2, Color.brown.darken2, Color.cyan.lighten2, Color.cyan.darken2, Color.deepOrange.lighten2, Color.deepOrange.darken2, Color.deepPurple.lighten2, Color.deepPurple.darken2, Color.green.lighten2, Color.green.darken2, Color.indigo.lighten2, Color.indigo.darken2, Color.lightBlue.lighten2, Color.lightBlue.darken2, Color.lime.lighten2, Color.lime.darken2, Color.pink.lighten2, Color.pink.darken2, Color.purple.lighten2, Color.purple.darken2, Color.red.lighten2, Color.red.darken2, Color.teal.lighten2, Color.teal.darken2, Color.yellow.lighten2, Color.yellow.darken2]
 
-    var gliphList: [UIImage] = []
+    var gliphList: [FontAwesomeIcon] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         for index in 0..<FontAwesomeIcon.count {
-            let image = Iconic.standardDimension(icon: FontAwesomeIcon.init(rawValue: index)!)
+            let image = FontAwesomeIcon(rawValue: index)!
             self.gliphList.append(image)
         }
 
@@ -72,8 +75,11 @@ final class BECategorySelectorViewController: UIViewController {
 
     @IBAction func saveAction(_ sender: Any) {
 
-        // Save currently selected into.. realm?
+        if let category = self.newCategory.toCategoryObject() {
 
+            BERealmManager.shared.saveCategory(category)
+            self.dismiss(animated: true, completion: nil)
+        }
     }
 
     @IBAction func cancelAction(_ sender: Any) {
@@ -116,36 +122,33 @@ extension BECategorySelectorViewController: UICollectionViewDataSource {
 
             case .gliph:
                 let cell = collectionView.dequeueReusableCell(withReuseIdentifier: R.reuseIdentifier.gliphReuseIdentifier, for: indexPath)!
-
-                cell.setImage(image: self.gliphList[indexPath.row])
+                let gliphList = self.gliphList[indexPath.row]
+                cell.setImage(image: Iconic.defaultDimension(icon: gliphList))
                 return cell
             }
         }
 
         fatalError()
     }
-
 }
+
 
 extension BECategorySelectorViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
 
         if let colorType = CategoryCollectionViewType.init(rawValue: collectionView.tag) {
+
             switch colorType {
+
             case .color:
                 // Selected color
                 self.newCategory.setColor(color: self.colorList[indexPath.row])
 
             case .gliph:
                 // Selected gliph
-                self.newCategory.setImage(image: self.gliphList[indexPath.row])
+                self.newCategory.setGliph(iconName: self.gliphList[indexPath.row].name)
+                
             }
         }
-    }
-
-    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-
-//        let cell = collectionView.cellForItem(at: indexPath)
-//        cell?.contentView.backgroundColor = .clear
     }
 }
